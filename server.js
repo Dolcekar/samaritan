ecef55746a3b6366ec37649b3b687b3152ab0e29
Carla Cox <astronomykar@gmail.com>
2018-09-25 21:16:00 -0400

    Initial setup and routes added

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const path = require("path");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const db = require('./config/keys').mondoURI;
+
+
+//Connect to MongoDB
+mongoose
+  .connect(db)
+  .then( ()=> console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+  const profile = require('./routes/api/profile');
+  const users = require('./routes/api/users');
+  const maps = require('./routes/api/maps');
+  const posts = require('./routes/api/posts');
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+app.use('/api/maps', maps);
+
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
