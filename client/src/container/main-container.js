edd4d9c2a947d824b772a5ecaeb0da98c3f1a7bc
Blonde Debbie <valerie.j.porter@gmail.com>
2018-09-27 22:10:42 -0400

    added react router

--- client/src/container/main-container.js
+++ client/src/container/main-container.js
@@ -1,21 +1,17 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import ProfileContainer from "../profile/profile-container";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import HelpRequestContainer from "../requests/help-requests-container";
 import HomeContainer from "../home/home-container";
 
 class MainContainer extends Component {
   render() {
     return (
-    <Router>
-      <div>
+      <Router>
+        <div>
           <Route exact path="/" component={HomeContainer} />
-          <Route path="/profile" component={ProfileContainer} />
           <Route path="/requests" component={HelpRequestContainer} />
-      </div>
-    </Router> 
-
-
+        </div>
+      </Router>
     );
   }
 }
--- client/src/home/home-container.js
+++ client/src/home/home-container.js
@@ -1,19 +1,26 @@
 import React, { Component } from "react";
-import { Button } from 'react-materialize';
+import { Button } from "react-materialize";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import ProfileContainer from "../profile/profile-container";
+import { CustomLink } from "../common/custom-link";
 
 class HomeContainer extends Component {
   render() {
+      /* ToDo: figure out how to make buttons disappear
+        https://reacttraining.com/react-router/web/example/custom-link
+      */
     return (
-      <div>
+      <Router>
+        <div>
           <p>
-              Homepage coming!
-              <Button waves='light'>sign in</Button>
-              <Button waves='light'>sign up</Button>
+            <Button waves="light">sign up</Button>
+            <Link to="/profile" activeOnlyWhenExact={true}>
+              <Button waves="light">sign in</Button>
+            </Link>
           </p>
-
-      </div>
-
-
+          <Route path="/profile" component={ProfileContainer} />
+        </div>
+      </Router>
     );
   }
 }
