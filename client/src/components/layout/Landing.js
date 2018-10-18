e11b4e0d40cf0a465d86aace63446040b18c52a0
Carla Cox <astronomykar@gmail.com>
2018-10-18 17:01:50 -0400

    mooore changes, finallyb things are looking normal again

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -17,11 +17,11 @@ class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Developer Connector</h1>
+                <h1 className="display-3 mb-4">The Samaritans</h1>
                 <p className="lead">
                   {' '}
-                  Create a developer profile/portfolio, share posts and get help
-                  from other developers
+                  Everyone needs help, everytime and everywhere. Create a Samaritan profile today and interact with a community of like-minded people, share posts and get help
+                  from other helpers.
                 </p>
                 <hr />
                 <Link to="/register" className="btn btn-lg btn-info mr-2">
