14130a650515280646a274102b7ffc080e95b92e
Carla Cox <astronomykar@gmail.com>
2018-10-18 16:44:57 -0400

    changes

---
+++ client/build/precache-manifest.5967e2f7ec3386c85f6ce8abd22b5bc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "185f93d9a67770120c7891a22af9840a",
+    "url": "/static/media/showcase.185f93d9.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "57bd924566fab79386aa",
+    "url": "/static/js/main.57bd9245.chunk.js"
+  },
+  {
+    "revision": "25ce9be85c80377af4f9",
+    "url": "/static/js/1.25ce9be8.chunk.js"
+  },
+  {
+    "revision": "57bd924566fab79386aa",
+    "url": "/static/css/main.312b104a.chunk.css"
+  },
+  {
+    "revision": "1d92b964f34ecc22d06f024edef1bd4f",
+    "url": "/index.html"
+  }
+];
